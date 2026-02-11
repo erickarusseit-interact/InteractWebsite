73c46b4c57a855c1a42b0f0227d9f144cedd2a6a
EricKarusseit <eric.karusseit@interact-connects.de>
2026-02-11 21:30:00 +0100

    neue Herso section und bug fixese

--- app/(Sections)/Hero.tsx
+++ app/(Sections)/Hero.tsx
@@ -5,9 +5,6 @@ export default function Hero() {
     return <section className='h-fit mx-auto max-w-[1440px] bg-black text-neutral-100 sm:px-8 pt-32 pb-30 lg:px-20 flex flex-col md:flex-row md:pt-32 md:pb-24 lg:pt-48 lg:pb-32 xl:flex-row items-center justify-between w-full gap-24 2xl:gap-0 overflow-hidden'>
     <div className='flex flex-col gap-4 items-center md:items-start w-full md:w-1/2 text-center md:text-left'>
         <div className='flex flex-col gap-2'>
- 
-       
-
         <h1 className='text-neutral-100 text-4xl sm:text-5xl lg:text-6xl xl:text-7xl'> Andere raten, Wir wissen</h1>
         <p className=" text-neutral-300 max-w-none lg:max-w-[85%] ">
             Wir entwickeln Webapps und Websites mit Wirkung, 
@@ -18,41 +15,8 @@ export default function Hero() {
         <ButtonCTA text="Projekt starten"/>
     </div>
 
-
-
-
-
-
-
-
-
         {/* Hero:Window.IL */}
         <div className='flex flex-col justify-center items-center w-full px-4 sm:px-6 md:px-0 md:w-1/2 gap-3 sm:gap-4'>
-
-            {/* Tag Pills Row 2 - Marktforschung & Development
-            <div className='flex justify-between items-start gap-3 w-fit h-9'>
-                <div className='flex items-center pr-3 gap-3 h-9 bg-[#0C0A09] border border-[#292524] rounded'>
-                    <div className='flex items-center justify-center w-9 h-9 bg-white/10 border border-[#292524]'>
-                        <img src="/images/Icons/Broot.svg" alt="Code" width="12" height="12"/>
-                    </div>
-                    <span>UIDesign</span>
-                </div>
-
-                <div className='flex items-center pr-3 gap-3 h-9 bg-[#0C0A09] border border-[#292524] rounded'>
-                    <div className='flex flex-col items-start p-3 gap-[10px] w-9 h-9 bg-white/10 border border-[#292524]'>
-                        <img src="/images/Icons/User.svg" alt="User" width="12" height="12"/>
-                    </div>
-                    <span className=''>Marktforschung</span>
-                </div>
-                <div className='flex items-center pr-3 gap-3 h-9 bg-[#0C0A09] border border-[#292524] rounded'>
-                    <div className='flex flex-col items-start p-3 gap-[10px] w-9 h-9 bg-white/10 border border-[#292524]'>
-                        <img src="/images/Icons/Code.svg" alt="Code" width="12" height="12"/>
-                    </div>
-                    <span className=''>Development</span>
-                </div>
-
-            </div>*/}
-          
           {/* Main Window Container */}
           <div className='flex flex-col items-start w-full max-w-[500px] md:max-w-none border border-[#292524] rounded-md overflow-visible'>
             
@@ -68,42 +32,37 @@ export default function Hero() {
 
                 {/* Visuals Container */}
                 <div className='flex flex-col justify-center items-center gap-3 w-full max-w-full overflow-visible'>
-
-
-
-
-              
               {/* Inner Window with ElectricBorder and Glow effect - Responsive */}
               <div className='relative w-full aspect-[2/1] min-h-[100px] sm:min-h-[140px] md:min-h-[160px] lg:min-h-[180px] overflow-visible'>
             
                 {/* Layer 1: Purple blur glow (z-index 1) */}
-                <div 
-                  className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(100%+25px)] h-[calc(100%+20px)] bg-purple-500/50 border  rounded-[14px] z-[1]'
+                <div
+                  className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(100%+25px)] h-[calc(100%+20px)] bg-purple-500/50   rounded-[14px] z-[1]'
                   style={{
                     filter: 'blur(80px)',
                     boxShadow: '0px 0px 4px #FFFFFF'
                   }}
                 />
-                
+
                 {/* Layer 2: White blur (z-index 2) */}
-                <div 
-                  className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-purple-300 border  rounded-[14px] z-[2]'
+                <div
+                  className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-purple-300   rounded-[14px] z-[2]'
                   style={{
                     filter: 'blur(20px)',
                     boxShadow: '0px 0px 4px #FFFFFF'
                   }}
                 />
-                
+
                 {/* Layer 3: Light purple blur (z-index 3) */}
                 <div
-                  className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[92%] h-[85%] bg-purple-50/100 border  rounded-[24px] z-[3]'
+                  className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[92%] h-[85%] bg-purple-50/100 rounded-[24px] z-[3]'
                   style={{
                     filter: 'blur(10px)'
                   }}
                 />
 
                   <div
-                      className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[80%] h-[58%] bg-purple-200/80 border rounded-[14px] z-[3]'
+                      className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[80%] h-[58%] bg-purple-200/80  rounded-[14px] z-[3]'
                       style={{
                           filter: 'blur(15px)'
                       }}
@@ -124,20 +83,8 @@ export default function Hero() {
               </div>
 
             </div>
-
-
-
           </div>
-
-              {/* Bottom Tabs - Responsive */}
-              <div className='flex items-start px-2 sm:px-4 md:px-6 gap-2 sm:gap-3 w-full h-6 sm:h-8 md:h-10 lg:h-12'>
-                  <div className='w-full h-full bg-white/10 border border-[#292524] rounded-t-[4px]'></div>
-                  <div className='w-full h-full bg-white/10 border border-[#292524] rounded-t-[4px]'></div>
-              </div>
         </div>
       </div>
-
-    
     </section>
-    
 }
