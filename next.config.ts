fe6d7f68a0088dbd9c6cfc5b7242426096908857
Eric K <erick@MacBook-Pro-von-Eric.local>
2026-04-09 11:06:49 +0200

    Add techstack updates and new logos

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     //reactCompiler: true,
     turbopackClientSideNestedAsyncChunking: true,
+    turbopackFileSystemCacheForDev: false,
   },
   /* config options here */
 };
